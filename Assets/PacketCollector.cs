d8d57155ec19e9c948dc2902cae8471286bd11ef
Pastew <kpastew@gmail.com>
2018-01-27 00:52:55 +0100

    Player input works

---
+++ Assets/PacketCollector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacketCollector : MonoBehaviour {
+
+    private enum Colors { Red, Green };
+    private ParticleSystem redParticleSystem, greenParticleSystem;
+
+    void Start()
+    {
+        redParticleSystem = transform.Find("RedParticleSystem").GetComponent<ParticleSystem>();
+        greenParticleSystem = transform.Find("GreenParticleSystem").GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Open() { SetColor(Colors.Green); }
+
+    public void Close() { SetColor(Colors.Red); }
+
+    private void SetColor(Colors color)
+    {
+        if (color == Colors.Red)
+        {
+            redParticleSystem.Play();
+            greenParticleSystem.Stop();
+        }
+        else
+        {
+            redParticleSystem.Stop();
+            greenParticleSystem.Play();
+        }
+    }
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    PacketCollector packetCollector;
+
+	void Start () {
+        packetCollector = FindObjectOfType<PacketCollector>();
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown("z"))
+            packetCollector.Open();
+        else if (Input.GetKeyDown("x"))
+            packetCollector.Close();
+    }
+}
